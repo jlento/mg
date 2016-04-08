959cc540c27079dfe608243bb78333ae6d6b2cbb
Juha Lento <juha.lento@csc.fi>
2016-04-08 15:00:02 +0300

    Red-Black Gauss-Seidel does not perform in MG as well as natural ordering GS?

--- src/mod_poisson_1d_GS.f90
+++ src/mod_poisson_1d_GS.f90
@@ -8,7 +8,7 @@ contains
   subroutine poisson_GS_iteration_1d ( x )
     type ( poisson_1d ), intent ( inout ) :: x
 
-    integer :: i
+    integer :: i, n
 
     associate ( &
          h => x % h, &
@@ -16,11 +16,20 @@ contains
          v => x % f )
 
       call x % force_boundaries
+      ! Natural ordering Gauss-Seidel
       do i = 2, size ( u ) - 1
          u ( i ) = 0.5_fp * ( h ** 2 * v ( i ) + u ( i - 1 ) + u ( i + 1 ) )
       end do
 
-  end associate
+      ! Red-Black Gauss-Seidel
+!      n = size ( x % u )
+!      u ( 2 : n - 1 : 2 ) = 0.5_fp * ( h ** 2 * v ( 2 : n - 1 : 2 ) &
+!           + u ( 1 : n - 2  : 2 ) + u ( 3 : n : 2 ) )
+!      if ( size ( u ) > 3 ) then
+!         u ( 3 : n - 1 : 2 ) = 0.5_fp * ( h ** 2 * v ( 3 : n - 1 : 2 ) &
+!              + u ( 2 : n - 2 : 2 ) + u ( 4 : n : 2 ) )
+!      end if
+    end associate
 
   end subroutine poisson_GS_iteration_1d
 
