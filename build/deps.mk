7c0c27bf466a41929e750ba9592e6e71589efd46
Juha Lento <juha.lento@csc.fi>
2016-06-07 17:01:53 +0300

    Minor fix

--- build/deps.mk
+++ build/deps.mk
@@ -1,4 +1,3 @@
-..o : ../src/makefile 
 poisson_1d_identity_test.o : ../src/poisson_1d_identity_test.F90 mod_poisson_1d_identity_test.o mod_defs.o 
 mod_defs.o : ../src/mod_defs.F90 
 mod_poisson_1d.o : ../src/mod_poisson_1d.F90 mod_defs.o 
